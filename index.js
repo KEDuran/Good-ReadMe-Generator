657d437d9fca90174f5c9f49d5c8441f513e87c2
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-06-17 22:22:43 -0500

    added all remaining readme questions into questions variabl in index.js

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
 // Const question variable that will house all the questions.
 const questions = [
-	// Question for project title
+	// Question for project title section
 	{
 		type: "input",
 		name: "Title",
 		message: "What is the title of your project?",
 	},
-	// Question for project description
+	// Question for Project Description section
 	{
 		type: "input",
 		name: "Description",
 		message: "Please enter a brief description of your project.",
-    },
-    // Question for installation
-    {
-        type:"input",
-        name: "Installation",
-        message: "What commands did you use to install the program?"
-    }
-    // 
+	},
+	// Question for Installation section
+	{
+		type: "input",
+		name: "Installation",
+		message: "What commands did you use to install the program?",
+	},
+	// Question for Usage Section
+	{
+		type: "input",
+		name: "Usage",
+		message: "Please describe how we can use this program.",
+	},
+	// Question for License section
+	{
+		type: "input",
+		name: "License",
+		message: "Please enter any license information for this project.",
+	},
+	// Question for Contributing section
+	{
+		type: "input",
+		name: "Contributing",
+		message: "How can a someone contribute to your project?",
+	},
+	// Question for Tests section
+	{
+		type: "input",
+		name: "Tests",
+		message:
+			"Please enter any testing protocols that you used for your project?",
+	},
+	// Profile pic question for Contact section
+	{
+		type: "input",
+		name: "profilePic",
+		message: "Please upload your GitHub profile picture?",
+	},
+	// Username question for Contact section
+	{
+		type: "input",
+		name: "userName",
+		message: "What is your GitHub username?",
+	},
+	// User email question for Contact section
+	{
+		type: "input",
+		name: "userEmail",
+		message: "What is your GitHub email?",
+	},
 ];
 
 function writeToFile(fileName, data) {}
