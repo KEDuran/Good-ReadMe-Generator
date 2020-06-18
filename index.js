404eef4baa5bd68e906950428e25977f05216736
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-06-17 21:57:18 -0500

    added first three user prompt messages for readme generator in index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+// Const question variable that will house all the questions.
 const questions = [
-
+	// Question for project title
+	{
+		type: "input",
+		name: "Title",
+		message: "What is the title of your project?",
+	},
+	// Question for project description
+	{
+		type: "input",
+		name: "Description",
+		message: "Please enter a brief description of your project.",
+    },
+    // Question for installation
+    {
+        type:"input",
+        name: "Installation",
+        message: "What commands did you use to install the program?"
+    }
+    // 
 ];
 
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
-function init() {
-
-}
+function init() {}
 
 init();
