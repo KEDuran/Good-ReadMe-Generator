9df88fdb3a1ffa4234fd7ed2c5a57b0e4f216da0
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-06-20 12:56:36 -0500

    added inquirer.prompt to index.js

--- index.js
+++ index.js
@@ -62,12 +62,15 @@ const questions = [
 		message: "What is your GitHub email?",
 	},
 ];
-
 // Const fs variable that will allow us to use fs modules
 const fs = require("fs");
+// Const inquirer variable that will allow us to use the NPM inquirer module
+const inquirer = require("inquirer");
 
 function writeToFile(fileName, data) {}
 
-function init() {}
+function init() {
+	inquirer.prompt(questions);
+}
 
 init();
