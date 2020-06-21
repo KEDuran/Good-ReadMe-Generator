29e34dc332abc545c60ac63f2b047f45c503dc9d
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-06-21 13:20:42 -0500

    added more license selection options, as well as licenseBadge ()function

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// This function will return specific badges based on license selection.
 function licenseBadge(value) {
-	if (value === "GPL v3") {
+	if (value === "GNU AGPLv3") {
+		return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+	} else if (value === "GNU GPLv3") {
 		return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-	} else if (value === "Apache 2.0") {
+	} else if (value === "GNU LGPLv3") {
+		return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+	} else if (value === "Mozilla") {
+		return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+	} else if (value === "Apache") {
 		return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
 	} else {
-		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+		return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
 	}
 }
 
@@ -58,7 +65,14 @@ const questions = [
 		type: "list",
 		name: "license",
 		message: "Please select the license you used for this project.",
-		choices: ["GPL v3", "Apache 2.0", "MIT"],
+		choices: [
+			"GNU AGPLv3",
+			"GNU GPLv3",
+			"GNU LGPLv3",
+			"Mozilla",
+			"Apache",
+			"Boost",
+		],
 		// validate inquirer method to make sure question is answered.
 		validate: validation,
 	},
